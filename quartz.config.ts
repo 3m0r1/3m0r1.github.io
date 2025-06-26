1ed077b3db35b9882e929da7848a30ae2beb43cc
3m0r1 <vxthird@gmail.com>
2025-06-26 22:29:12 +0300

    Quartz sync: Jun 26, 2025, 10:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "3m0r1.github.io/Blog",
+    baseUrl: "3m0r1.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
